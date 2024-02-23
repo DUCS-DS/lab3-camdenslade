9c5778e69e40ce46a63d8d253d1b66834536ec40
camdenslade <camdenslade@outlook.com>
2024-02-23 11:42:22 -0600

    Added llist

--- llist.py
+++ llist.py
@@ -1,18 +1,27 @@
 class Node:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+    def __init__(lst, val, nextNode=None):
+        lst.val = val
+        lst.next = nextNode
 
 class LList:
-    def __init__(self):
-        self.head = None
+    def __init__(lst):
+        lst.head = None
+        lst.tail = None
 
-def append(lst, newnode):
-    """append newnode to a finite linked list lst"""
-    if lst.head:  # same as:  if lst.head is not None
-        node = lst.head
-        while node.next:
-            node = node.next
-        node.next = newnode
-    else:
-        lst.head = newnode
+    def append(lst, val):
+        newNode = Node(val)
+        if not lst.head:
+            lst.head = newNode
+            lst.tail = newNode
+        else:
+            lst.tail.next = newNode
+            lst.tail = newNode
+
+    def prepend(lst, value):
+        new_node = Node(value)
+        if not lst.head:
+            lst.head = new_node
+            lst.tail = new_node
+        else:
+            new_node.next = lst.head
+            lst.head = new_node
